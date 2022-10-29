9301d4cffdfc9e5475a19e5f546f1ba4cc9f4975
codingvaleria <codingvaleria@gmail.com>
2022-10-29 09:31:29 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES 
+('Mr. Chocolate', 8, 'M', 'BLACK', 'NEUTRAL', 0),
+('Rowdy', 7, 'M', 'WHITE', 'AGGRESSIVE', 1),
+('Tabitha', 6, 'F', 'BLACK', 'PASSIVE', 0),
+('Sergeant Brown', 5, 'M', 'WHITE', 'NEUTRAL', 1),
+('Melissa', 4, 'F', 'BLACK', 'PASSIVE', 0),
+('Grinch', 3, 'M', 'WHITE', 'NEUTRAL', 1),
+('Wendy', 2, 'F', 'BLACK', 'PASSIVE', 0 ),
+(NULL, 1, 'M', 'WHITE', 'AGGRESSIVE', 1 );
+
+
+
+
